fe8b26ff8090c55cb075b8ffef90d6d0941116e2
sairayal2427 <sairayal@amazon.com>
2023-04-08 15:30:00 +0530

    java code

---
+++ src/test/java/com/amazon/amazonjenkinsdemo/AmazonJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.amazonjenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonJenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
